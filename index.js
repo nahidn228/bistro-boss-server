d4085bcf1de3a021507bee95cc7956af3b791745
Nahid Hasan <nahidn228@gmail.com>
2025-01-02 08:58:37 +0600

    create cartCollection delete API

--- index.js
+++ index.js
@@ -41,9 +41,21 @@ async function run() {
 
     // Carts Collection
 
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
